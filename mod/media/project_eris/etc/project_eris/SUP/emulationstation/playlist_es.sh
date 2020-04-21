e9c481171a75ec0de612dfdfda1439fba5f7b0d2
DefKorns <defkorns@gmail.com>
2020-04-21 01:22:23 +0000

    gitignor + clean playlist with multi disc roms, added more rom support

--- mod/media/project_eris/etc/project_eris/SUP/emulationstation/playlist_es.sh
+++ mod/media/project_eris/etc/project_eris/SUP/emulationstation/playlist_es.sh
@@ -16,47 +16,74 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-gaaData="/gaadata/"
-peGameDir="/media/games/"
-esPSXDir="/media/roms/psx"
-volPSX="/var/volatile${esPSXDir}"
-m3uDir="/media/project_eris/etc/project_eris/SUP/retroarch"
-region="$(cat "/gaadata/geninfo/REGION")"
-palGames=(SLUS-00594 SLUS-00776 SCUS-94163 SCUS-94164 SCUS-94165)
-ntscjGames=(SLPS-01230 SLPS-01231 SLPS-01057 SLPS-01058 SLPS-01059 SLPM-86114 SLPM-86115)
+tmp_dir="/var/volatile"
+original_games="/gaadata"
+custom_games="/media/games"
+rom_dir="/media/roms/psx"
+lower_dir="${tmp_dir}${rom_dir}"
+original_m3u="/media/project_eris/etc/project_eris/SUP/retroarch"
+psc_region="$(cat "${original_games}/geninfo/REGION")"
+pal_game_ids=(SLUS-00594 SLUS-00776 SCUS-94163 SCUS-94164 SCUS-94165)
+ntscj_game_ids=(SLPS-01230 SLPS-01231 SLPS-01057 SLPS-01058 SLPS-01059 SLPM-86114 SLPM-86115)
+western_psc=(EK UC)
 
 # check if overlay is mounted and unmount it
-mount | grep -qs "${esPSXDir}" && umount "${esPSXDir}"
+mount | grep -qs "${rom_dir}" && umount "${rom_dir}"
+
+# fix this
+find ${rom_dir} -type l -exec rm {} \;
+[ -d "${lower_dir}" ] && rm -rf "${lower_dir}"
 
 # create lowerdir on volatile
-[ ! -d "${volPSX}" ] && mkdir -p "${volPSX}"
+[ ! -d "${lower_dir}" ] && mkdir -p "${lower_dir}"
+[ -f "${tmp_dir}/psxCleanup" ] && rm -rf "${tmp_dir}/psxCleanup"
 
 # find all PSX games and link them to ES rom folder
+find "$original_games" "$custom_games" -maxdepth 2 -regex ".*\.\(cue\|m3u\|bin\|pbp\|chd\|iso\|cbn\|ccd\|img\|mdf\|toc\|z\|znx\)" | while IFS= read -r game_file; do
+  file_name="${game_file##*/}"
+  game_title=${file_name%.*}
+  ext=${game_file##*.}
+  game_path="${game_file%/*}"
 
-find "$gaaData" "$peGameDir" -maxdepth 2 -regex ".*\.\(cue\|m3u\|bin\|pbp\|chd\|iso\)" | while IFS= read -r gameFile; do
-  gameTitle="${gameFile##*/}"
+  if [ ! -f "${lower_dir}/${file_name}" ]; then
+    ln -s "$game_file" "$lower_dir"
+  fi
 
-  if [ ! -f "${volPSX}/${gameTitle}" ]; then
-    ln -s "$gameFile" "$volPSX"
+  if [ "${ext}" == "m3u" ]; then
+    echo "${game_title}" >>"${tmp_dir}/psxCleanup"
+    find "${game_path}" -name "*.cue" >"${lower_dir}/${file_name}"
   fi
 done
+# DefKorns Only
+#     echo "Resident Evil 2 (Europe)
+# Parasite Eve (USA)
+# Parasite Eve II (USA)
+# Policenauts (Japan)
+# Heart of Darkness (USA)
+# Final Fantasy IX
+# Final Fantasy VIII (USA)
+# Chrono_Cross" >>"${tmp_dir}/psxCleanup"
 
 # Load playlists for multidisc original games
-if [ "$region" == "EK" ]; then
-  for g in "${palGames[@]}"; do
-    find "$volPSX" -name "${g}.*" -exec rm {} +
+if [[ "${western_psc[@]}" =~ "$psc_region" ]]; then
+  for g in "${pal_game_ids[@]}"; do
+    find "$lower_dir" -name "${g}.*" -exec rm {} +
   done
-  ln -s "${m3uDir}/SLUS-00594.m3u" "${volPSX}"
-  ln -s "${m3uDir}/SCUS-94163.m3u" "${volPSX}"
+  ln -s "${original_m3u}/SLUS-00594.m3u" "${lower_dir}"
+  ln -s "${original_m3u}/SCUS-94163.m3u" "${lower_dir}"
 else
-  for g in "${ntscjGames[@]}"; do
-    find "$volPSX" -name "${g}.*" -exec rm {} +
+  for g in "${ntscj_game_ids[@]}"; do
+    find "$lower_dir" -name "${g}.*" -exec rm {} +
   done
-  ln -s "${m3uDir}/SLPS-01230.m3u" "${volPSX}"
-  ln -s "${m3uDir}/SLPS-01057.m3u" "${volPSX}"
-  ln -s "${m3uDir}/SLPM-86114.m3u" "${volPSX}"
+  ln -s "${original_m3u}/SLPS-01230.m3u" "${lower_dir}"
+  ln -s "${original_m3u}/SLPS-01057.m3u" "${lower_dir}"
+  ln -s "${original_m3u}/SLPM-86114.m3u" "${lower_dir}"
 fi
 
+while IFS= read -r game; do
+  find "${lower_dir}" \( -name "${game}*.cue" -o -name "${game}*.bin" \) -exec rm {} +
+done <"${tmp_dir}/psxCleanup"
+
 # mount volume
-mount -t overlay -o lowerdir="${esPSXDir}":"${volPSX}" "overlay-PSX2ES-media" "${esPSXDir}"
+mount -t overlay -o lowerdir="${rom_dir}":"${lower_dir}" "overlay-PSX2ES-media" "${rom_dir}"
 # EOF
